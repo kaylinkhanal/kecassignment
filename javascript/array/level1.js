e7eabcfc335d767445144981151be931e94e2445
Kaylin Khanal <kyalinnop@gmail.com>
2024-06-04 17:23:26 +0545

    Added string question

--- javascript/array/level1.js
+++ javascript/array/level1.js
@@ -2,15 +2,15 @@
 
 const arr = [3,5,1,2,9,12]
 //write your code here
-console.log(arr)
+
 
 //expected output---> [3,9,12]
 // hint: %3 === 0
 // filter
 //syntax:
-// arr.filter((item,id)=>{
+//const filteredArr = arr.filter((item,id)=>{
 //     //
 // })
-
+//console.log(filteredArr)
 
 //----------------
---
+++ javascript/array/level2.js
@@ -0,0 +1,11 @@
+const arr = ['kaylin', 'ram', 'gopal', 'ankit']
+
+const userDetails = {
+    'kaylin': 'khanal',
+    'alisha': 'rauniyar',
+    'ayush': 'rai',
+    'gopal': 'tharu'
+}
+
+// expected output :
+// ['kaylin khanal', 'gopal tharu']
