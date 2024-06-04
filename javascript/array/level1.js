62d0a8d407b53ac1b853b674108a8e6c6b214f05
Kaylin Khanal <kyalinnop@gmail.com>
2024-06-04 17:08:52 +0545

    Added string question

---
+++ javascript/array/level1.js
@@ -0,0 +1,16 @@
+//Q1 find list of numbers divisible by 3
+
+const arr = [3,5,1,2,9,12]
+//write your code here
+console.log(arr)
+
+//expected output---> [3,9,12]
+// hint: %3 === 0
+// filter
+//syntax:
+// arr.filter((item,id)=>{
+//     //
+// })
+
+
+//----------------
--- javascript/string/level1.js
+++ javascript/string/level1.js
@@ -3,3 +3,5 @@ const user = 'ram thapa'
 
 console.log(user)
 //RAM THAPA
+
+
